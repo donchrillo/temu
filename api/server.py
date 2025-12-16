cd9f70be962d8b784d7373b3655279c09e55a72b
Christian Blass <blass@toci.de>
2025-12-16 12:54:12 +0100

    Kleine Änderungen am Sytem NAchdem diverse Änderungen vorgenommen wurden, um die Leistung zu verbessern und Fehler zu beheben, sind hier die wichtigsten Updates: klappte nichts mehr und ich habe diesen STage wieder hergestellt

--- api/server.py
+++ api/server.py
@@ -271,7 +271,7 @@ else:
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "api.main:app",
+        "api.server:app",
         host="0.0.0.0",
         port=8000,
         reload=True
